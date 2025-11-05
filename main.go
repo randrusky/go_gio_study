883805de2ee5484ecd12672727622e02a01f8c7c
Andrii Rakutskyi <andru.rakutsky@gmail.com>
2025-11-05 12:42:16 +0200

    Add main window

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "gioui.org/app"
+)
+
+func main() {
+    go func() {
+        // create new window
+        w := new(app.Window)
+
+        // listen for events in the window
+        for {
+            w.Event()
+        }
+    }()
+    app.Main()
+}
+
