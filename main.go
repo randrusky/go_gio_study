a0eb1de850fc95e4c38da87688e0588e2a7b016f
Andrii Rakutskyi <andru.rakutsky@gmail.com>
2025-11-05 12:49:56 +0200

    Add button

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-  "gioui.org/app"
-  "gioui.org/unit"
+    "os"
+
+    "gioui.org/app"
+    "gioui.org/op"
+    "gioui.org/unit"
+    "gioui.org/widget"
+    "gioui.org/widget/material"
 )
+
 
 func main() {
   go func() {
@@ -12,10 +18,36 @@ func main() {
 		w.Option(app.Title("Egg timer"))
 		w.Option(app.Size(unit.Dp(400), unit.Dp(600)))
 
-    // listen for events in the window
+    // ops are the operations from the UI
+    var ops op.Ops
+
+    // startButton is a clickable widget
+    var startButton widget.Clickable
+
+    // th defines the material design style
+    th := material.NewTheme()
+
+    // listen for events in the window.
     for {
-        w.Event()
+        // first grab the event
+        evt := w.Event()
+
+        // then detect the type
+        switch typ := evt.(type) {
+
+        // this is sent when the application should re-render.
+        case app.FrameEvent:
+            gtx := app.NewContext(&ops, typ)
+            btn := material.Button(th, &startButton, "Start")
+            btn.Layout(gtx)
+            typ.Frame(gtx.Ops)
+
+        // and this is sent when the application should exits
+        case app.DestroyEvent:
+            os.Exit(0)
+        }
     }
   }()
   app.Main()
 }
+
