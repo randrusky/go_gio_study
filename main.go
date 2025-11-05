f9d573c4ff9a8875d0b62d14a4ec91f058882707
Andrii Rakutskyi <andru.rakutsky@gmail.com>
2025-11-05 12:44:06 +0200

    Add title

--- main.go
+++ main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-    "gioui.org/app"
+  "gioui.org/app"
+  "gioui.org/unit"
 )
 
 func main() {
-    go func() {
-        // create new window
-        w := new(app.Window)
+  go func() {
+    // create new window
+		w := new(app.Window)
+		w.Option(app.Title("Egg timer"))
+		w.Option(app.Size(unit.Dp(400), unit.Dp(600)))
 
-        // listen for events in the window
-        for {
-            w.Event()
-        }
-    }()
-    app.Main()
+    // listen for events in the window
+    for {
+        w.Event()
+    }
+  }()
+  app.Main()
 }
-
