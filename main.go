555b3a10b02a41ef32f46c31edafc2fe8545ef19
Andrii Rakutskyi <andru.rakutsky@gmail.com>
2025-11-07 13:58:20 +0200

    Add egg

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"image"
 	"image/color"
 	"log"
+	"math"
 	"os"
 	"time"
 
 	"gioui.org/app"
+	"gioui.org/f32"
 	"gioui.org/layout"
 	"gioui.org/op"
 	"gioui.org/op/clip"
@@ -90,19 +92,47 @@ func draw(w *app.Window) error {
 				// Empty space is left at the start, i.e. at the top
 				Spacing: layout.SpaceStart,
 			}.Layout(gtx,
+
 				layout.Rigid(
 					func(gtx C) D {
-						circle := clip.Ellipse{
-							// Hard coding the x coordinate. Try resizing the window
-							// Min: image.Pt(80, 0),
-							// Max: image.Pt(320, 240),
-							// Soft coding the x coordinate. Try resizing the window
-							Min: image.Pt(gtx.Constraints.Max.X/2-120, 0),
-							Max: image.Pt(gtx.Constraints.Max.X/2+120, 240),
-						}.Op(gtx.Ops)
-						color := color.NRGBA{R: 200, A: 255}
-						paint.FillShape(gtx.Ops, color, circle)
-						d := image.Point{Y: 400}
+						// Draw a custom path, shaped like an egg
+						var eggPath clip.Path
+						op.Offset(image.Pt(gtx.Dp(200), gtx.Dp(125))).Add(gtx.Ops)
+						eggPath.Begin(gtx.Ops)
+						// Rotate from 0 to 360 degrees
+						for deg := 0.0; deg <= 360; deg++ {
+
+							// Egg math (really) at this brilliant site. Thanks!
+							// https://observablehq.com/@toja/egg-curve
+							// Convert degrees to radians
+							rad := deg * math.Pi / 180
+							// Trig gives the distance in X and Y direction
+							cosT := math.Cos(rad)
+							sinT := math.Sin(rad)
+							// Constants to define the eggshape
+							a := 110.0
+							b := 150.0
+							d := 20.0
+							// The x/y coordinates
+							x := a * cosT
+							y := -(math.Sqrt(b*b-d*d*cosT*cosT) + d*sinT) * sinT
+							// Finally the point on the outline
+							p := f32.Pt(float32(x), float32(y))
+							// Draw the line to this point
+							eggPath.LineTo(p)
+						}
+						// Close the path
+						eggPath.Close()
+
+						// Get hold of the actual clip
+						eggArea := clip.Outline{Path: eggPath.End()}.Op()
+
+						// Fill the shape
+						// color := color.NRGBA{R: 255, G: 239, B: 174, A: 255}
+						color := color.NRGBA{R: 255, G: uint8(239 * (1 - progress)), B: uint8(174 * (1 - progress)), A: 255}
+						paint.FillShape(gtx.Ops, color, eggArea)
+
+						d := image.Point{Y: 375}
 						return layout.Dimensions{Size: d}
 					},
 				),
